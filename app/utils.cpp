7968efdcaedcb47fbe656bd3a3f8c79129296fdb
Benjamin <benjamin.humeau@epsi.fr>
2015-04-22 17:42:21 +0200

    Finish de calgonit

--- app/utils.cpp
+++ app/utils.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-double Utils::calculateForceTraction(double mc, double g, double beta, double mp, double alpha) {
-    return (mc * g) * sin(deg2rad(beta)) - (mp * g) * cos(deg2rad(alpha));
+double Utils::calculateForceTraction(double masseContrePoids, double gravite, double beta, double masseProjectile, double alpha) {
+    return (masseContrePoids * gravite) * sin(deg2rad(beta)) - (masseProjectile * gravite) * cos(deg2rad(alpha));
 }
 
 double Utils::calculateMomentBras(double forceTraction, double longueurBras) {
@@ -20,16 +20,29 @@ double Utils::calculateAcceleration(double momentBras, double momentInertie) {
     return momentBras / momentInertie;
 }
 
-double Utils::calculatePortee() {
-    return 0.0;
+double Utils::calculateVelocite(double acceleration, double longueurBras) {
+    return acceleration * longueurBras;
 }
 
-double Utils::calculateEnergieImpact() {
-    return 0.0;
+double Utils::calculatePortee(double velocite, double gravite, double alpha) {
+    double div = pow(velocite, 2) / gravite;
+
+    return div * sin(2 * (90 - alpha));
 }
 
-double Utils::calculateViabilite() {
-    return 0.0;
+double Utils::calculateEnergieImpact(double masseProjectile, double velocite) {
+    return 1 / 2 * masseProjectile * pow(velocite, 2);
+}
+
+bool Utils::calculateViabilite(double alpha, double longueurBras, double longueurBase, double masseProjectile, double masseContrePoids) {
+    double calc111 = pow(sin(alpha) * longueurBras, 2);
+    double calc112 = pow(cos(alpha) * longueurBras - longueurBase, 2);
+    double calc110 = calc101 + calc102;
+    double calc120 = sin(alpha) * (masseProjectile * gravite);
+    double calc100 = calc110 * calc120;
+    double calc200 = longueurBase * (masseContrePoids * gravite);
+
+    return calc100 <= calc200;
 }
 
 double Utils::deg2rad(double deg) {
--- app/utils.h
+++ app/utils.h
@@ -5,14 +5,14 @@
 
 class Utils {
 public:
-    double calculateForceTraction(double mc, double g, double beta, double mp, double alpha);
+    double calculateForceTraction(double masseContrePoids, double gravite, double beta, double masseProjectile, double alpha);
     double calculateMomentBras(double forceTraction, double longueurBras);
     double calculateInertieBras(double masseBras, double longueurBras);
     double calculateAcceleration(double momentBras, double momentInertie);
-    double calculateVelocite();
-    double calculatePortee();
-    double calculateEnergieImpact();
-    double calculateViabilite();
+    double calculateVelocite(double acceleration, double longueurBras);
+    double calculatePortee(double velocite, double gravite, double alpha);
+    double calculateEnergieImpact(double masseProjectile, double velocite);
+    bool calculateViabilite(double alpha, double longueurBras, double longueurBase, double masseProjectile, double masseContrePoids);
 private:
     double deg2rad(double deg);
 };
